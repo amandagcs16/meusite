26e560f0c39cebd55eebae23b7a79b0cf62905e8
amandagcs16 <amandagabrielasantos@yahoo.com.br>
2025-07-12 14:08:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("botao").addEventListener("click", function() {
+    const mensagem = document.getElementById("mensagem");
+    mensagem.textContent = "Cada dia é uma nova chance de recomeçar.";
+});
